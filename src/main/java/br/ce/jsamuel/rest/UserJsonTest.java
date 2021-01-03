d2abf221fa46aa92576e867f88939d58dd4cde9b
Jose Samuel <jose.samuel@dcx.ufpb.br>
2021-01-03 14:52:49 -0300

    Testando mensagem de erro

--- src/main/java/br/ce/jsamuel/rest/UserJsonTest.java
+++ src/main/java/br/ce/jsamuel/rest/UserJsonTest.java
@@ -78,7 +78,27 @@ public class UserJsonTest {
 			.body("filhos.name", hasItem("Zezinho"))
 			.body("filhos.name", hasItems("Zezinho", "Luizinho"))
 			;
+	}
+	
+	@Test
+	public void deveRetornarErroUsuarioInexistente() {
+		
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users/4")
+		.then()
+			.statusCode(404)
+			.body("error", is("Usuário inexistente"))
+			;
 		
 	}
 
 }
+
+
+
+
+
+
+
+
