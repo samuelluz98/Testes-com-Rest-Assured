3bf3623673104eafe11cc82d921182d1be71a4aa
Jose Samuel <jose.samuel@dcx.ufpb.br>
2021-01-22 16:00:59 -0300

    Unindo Json com Java Path

--- src/main/java/br/ce/jsamuel/rest/UserJsonTest.java
+++ src/main/java/br/ce/jsamuel/rest/UserJsonTest.java
@@ -3,6 +3,9 @@ package br.ce.jsamuel.rest;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -90,6 +93,69 @@ public class UserJsonTest {
 			.statusCode(404)
 			.body("error", is("Usuário inexistente"))
 			;
+		
+	}
+	
+	@Test
+	public void deveVerificarListaNaRaiz() {
+		
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(200)
+			.body("$", hasSize(3))
+			.body("name", hasItems("João da Silva", "Maria Joaquina", "Ana Júlia"))
+			.body("age[1]", is(25))
+			.body("filhos.name", hasItem(Arrays.asList("Zezinho", "Luizinho")))
+			.body("salary", contains(1234.5677f, 2500, null))
+			;
+		
+	}
+	
+	@Test
+	public void devoFazerVerificacoesAvancadas() {
+		
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(200)
+			.body("$", hasSize(3))
+			.body("age.findAll{it <= 25}.size()", is(2))
+			.body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+			.body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+			.body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
+			.body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
+			.body("find{it.age <= 25}.name", is("Maria Joaquina"))
+			.body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+			.body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
+			.body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+			.body("age.collect{it * 2}", hasItems(60, 50))
+			.body("id.max()", is(3))
+			.body("salary.min()", is(1234.5678f))
+			.body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678f, 0.001)))
+			.body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d), lessThan(5000d)))
+			;
+		
+	}
+	
+	@Test
+	public void devoUnirJsonPathComJAVA() {
+		
+		ArrayList<String> names =
+			given()
+			.when()
+				.get("http://restapi.wcaquino.me/users")
+			.then()
+				.statusCode(200)
+				.extract().path("name.findAll{it.startsWith('Maria')}")
+				;	
+		Assert.assertEquals(1, names.size());
+		Assert.assertTrue(names.get(0).equalsIgnoreCase("maria joaquina"));
+		Assert.assertEquals(names.get(0).toUpperCase(), "maria joaquina".toUpperCase());
 		
 	}
 
