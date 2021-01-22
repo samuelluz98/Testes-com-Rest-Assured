f3e278d65a4803c537f17cfe0c66793e6c0fb9b6
Jose Samuel <jose.samuel@dcx.ufpb.br>
2021-01-22 18:54:51 -0300

    Usando XML

---
+++ src/main/java/br/ce/jsamuel/rest/UserXMLTest.java
@@ -0,0 +1,29 @@
+package br.ce.jsamuel.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+public class UserXMLTest {
+	
+	@Test
+	public void devoTrabalharComXML() {
+		
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/usersXML/3")
+		.then()
+			.statusCode(200)
+			.body("user.name", is("Ana Julia"))
+			.body("user.@id", is("3"))
+			.body("user.filhos.name.size()", is(2))
+			.body("user.filhos.name[0]", is("Zezinho"))
+			.body("user.filhos.name[1]", is("Luizinho"))
+			.body("user.filhos.name", hasItem("Luizinho"))
+			.body("user.filhos.name", hasItems("Luizinho", "Zezinho"))
+		;
+		
+	}
+
+}
