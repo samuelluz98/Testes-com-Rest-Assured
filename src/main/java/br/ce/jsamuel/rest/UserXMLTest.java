1b619e32bbac6a0396f697a9cfad25019dc555a6
Jose Samuel <jose.samuel@dcx.ufpb.br>
2021-01-22 19:11:15 -0300

    Trabalhando com Nó Raiz

--- src/main/java/br/ce/jsamuel/rest/UserXMLTest.java
+++ src/main/java/br/ce/jsamuel/rest/UserXMLTest.java
@@ -15,13 +15,21 @@ public class UserXMLTest {
 			.get("http://restapi.wcaquino.me/usersXML/3")
 		.then()
 			.statusCode(200)
-			.body("user.name", is("Ana Julia"))
-			.body("user.@id", is("3"))
-			.body("user.filhos.name.size()", is(2))
-			.body("user.filhos.name[0]", is("Zezinho"))
-			.body("user.filhos.name[1]", is("Luizinho"))
-			.body("user.filhos.name", hasItem("Luizinho"))
-			.body("user.filhos.name", hasItems("Luizinho", "Zezinho"))
+			
+			.rootPath("user")
+			.body("name", is("Ana Julia"))
+			.body("@id", is("3"))
+			
+			.rootPath("user.filhos")
+			.body("name.size()", is(2))
+			
+			.detachRootPath("filhos")
+			.body("filhos.name[0]", is("Zezinho"))
+			.body("filhos.name[1]", is("Luizinho"))
+			
+			.appendRootPath("filhos")
+			.body("name", hasItem("Luizinho"))
+			.body("name", hasItems("Luizinho", "Zezinho"))
 		;
 		
 	}
